9867226d67da4c42af212c88b1fca8dd3e184185
Omachonu Prosper <omachonucodes@gmail.com>
2023-11-02 12:55:02 +0100

    Create app templates

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/events', strict_slashes=False)
+def events():
+    return render_template('events.html')
+
+
+@app.route('/event/<id>', strict_slashes=False)
+def event(id):
+    return render_template('event.html')
+
+
+@app.route('/event/<id>/purchase-ticket')
+def buy_ticket(id):
+    return render_template('ticket.html')
+
+
+@app.route('/', strict_slashes=False)
 def index():
-    return 'Welcome home'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
